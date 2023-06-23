b4e6b228ea06d3d27fdc30ce43f768fa3a5d9f11
oterte <97093479+oterte@users.noreply.github.com>
2023-06-23 20:42:52 +0900

    관리자, 유저페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      누구나 볼 수 있는 페이지 입니다.
     </main>
   )
 }
