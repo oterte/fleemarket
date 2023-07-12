41946018d0e1593c690ad54f4cd487f2650dcc4f
oterte <97093479+oterte@users.noreply.github.com>
2023-07-12 18:29:05 +0900

    ProductCard, HeartButton 컴포넌츠 생성

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import { Product, User } from "@prisma/client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+import HeartButton from "./HeartButton";
 
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+interface ProductCardProps {
+  data: Product;
+  currentUser?: User | null;
 }
 
-export default ProductCard
+const ProductCard = ({ data, currentUser }: ProductCardProps) => {
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push(`/products/data.id`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col w-full gap-2">
+        <div className="relative w-full overflow-hidden aspect-square rounded-xl">
+          <Image
+            fill
+            sizes="auto"
+            className="object-cover w-full h-full transition group-hover:scale-110"
+            src={data.imageSrc}
+            alt="Listing"
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton productId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className="text-lg font-semibold">{data.title}</div>
+        <div className="font-light text-netral-500">{data.category}</div>
+        <div className="flex flex-row items-center justify-between gap-1">
+          <div className="font-semibold">
+            {data.price} <span className="font-light">원</span>
+          </div>
+          <div>{/* {fromNow(data.createdAt)} */}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
