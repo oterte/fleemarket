8d9a0320ddb2aa57f207dd911bf35d76d8882848
oterte <97093479+oterte@users.noreply.github.com>
2023-07-13 18:00:28 +0900

    delete api 추가

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -35,6 +35,29 @@ export async function POST(request: Request, { params }: { params: Params }) {
 }
 
 
-export async function DELETE() {
-    
+export async function DELETE(request: Request, { params }: { params: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { productId } = params;
+
+  if (!productId || typeof productId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== productId)
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  })
+  return NextResponse.json(user);
 }
