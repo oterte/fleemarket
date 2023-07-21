4806f41dcd22eb7bfd6bee48c1126ea724df748b
oterte <97093479+oterte@users.noreply.github.com>
2023-07-21 12:22:32 +0900

    상품 상세페이지 ui 생성

--- src/app/products/[productId]/ProductClient.tsx
+++ src/app/products/[productId]/ProductClient.tsx
@@ -1,5 +1,11 @@
 "use client";
+import Button from "@/components/Button";
+import Container from "@/components/Container";
+import ProductHead from "@/components/products/ProductHead";
+import ProductInfo from "@/components/products/ProductInfo";
 import { Product, User } from "@prisma/client";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ProductClientProps {
@@ -10,7 +16,38 @@ interface ProductClientProps {
 }
 
 const ProductClient = ({ product, currentUser }: ProductClientProps) => {
-  return <div>ProductClient</div>;
+  const router = useRouter();
+  const KakaoMap = dynamic(() => import("../../../components/KakaoMap"), {
+    ssr: false,
+  });
+
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ProductHead />
+          <div 
+            className="grid grid-cols-1 mt-6 md:grid-cols-2 md:gap-10"
+          >
+            <ProductInfo />
+            <div>
+              <KakaoMap
+                detailPage
+                latitude={product.latitude}
+                longitude={product.longitude}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <Button
+            label="이 유저와 채팅하기"
+            onClick={() => router.push("/chats")}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductClient;
---
+++ src/components/products/ProductHead.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductHead = () => {
+  return (
+    <div>ProductHead</div>
+  )
+}
+
+export default ProductHead
