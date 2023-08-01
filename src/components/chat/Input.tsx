024a213e0154faf5974ba6e3271fda42778cce0f
oterte <97093479+oterte@users.noreply.github.com>
2023-08-01 14:52:27 +0900

    useSWRMutation을 이용해 채팅 작성하자마자 바로 적용

--- src/components/chat/Input.tsx
+++ src/components/chat/Input.tsx
@@ -2,24 +2,51 @@ import axios from "axios";
 import React, { FormEvent, useState } from "react";
 import { IoImageOutline } from "react-icons/io5";
 import { RiSendPlaneLine } from "react-icons/ri";
+import useSWRMutation from "swr/mutation";
 interface InputProps {
   receiverId: string;
   currentUserId: string;
 }
 
+const sendRequest = (
+  url: string,
+  {
+    arg,
+  }: {
+    arg: {
+      text: string;
+      image: string;
+      receiverId: string;
+      senderId: string;
+    };
+  }
+) => {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(arg),
+  }).then((res) => res.json);
+};
+
 const Input = ({ receiverId, currentUserId }: InputProps) => {
   const [message, setMessage] = useState("");
+  const { trigger } = useSWRMutation("/api/chat", sendRequest);
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const imageUrl = "";
     if (message || imageUrl) {
       try {
-        await axios.post("/api/chat", {
+        trigger({
           text: message,
           image: imageUrl,
           receiverId: receiverId,
           senderId: currentUserId,
-        });
+        })
+        // await axios.post("/api/chat", {
+        //   text: message,
+        //   image: imageUrl,
+        //   receiverId: receiverId,
+        //   senderId: currentUserId,
+        // });
       } catch (error) {
         console.error(error);
       }
